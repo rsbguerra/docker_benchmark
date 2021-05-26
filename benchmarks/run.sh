4451b28ac0050dd8f1a994efbc6454d631ca3a49
rsbguerra <rsbguerra@gmail.com>
2021-05-26 14:32:56 +0100

    Save iozone output as .xls
    Also uncommented code

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -4,11 +4,11 @@
     GEEKBENCH_PATH='/opt/geekbench/Geekbench-5.4.1-Linux'
     RES_PATH='./results'
 
-    #iozone -t 2 > $RES_PATH/iozone3
+    iozone -t 2 -b iozone_res > $RES_PATH/iozone3
 
-    #$RAMSPEED_PATH/ramsmp -b 3 > $RES_PATH/ramspeed3
-    #$RAMSPEED_PATH/ramsmp -b 6 > $RES_PATH/ramspeed6
-    #$GEEKBENCH_PATH/geekbench5   > $RES_PATH/geekbench
+    $RAMSPEED_PATH/ramsmp -b 3 > $RES_PATH/ramspeed3
+    $RAMSPEED_PATH/ramsmp -b 6 > $RES_PATH/ramspeed6
+    $GEEKBENCH_PATH/geekbench5 > $RES_PATH/geekbench
 
     iperf3 -s &
     iperf3 -c 127.0.0.1 -w 20K > $RES_PATH/iperf3_tcp
